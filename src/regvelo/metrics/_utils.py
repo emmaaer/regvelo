31187f418cda2ebd0f6f28d0f492b25d011eac2e
Emma Ryhänen <80543771+emmaaer@users.noreply.github.com>
2026-07-31 20:33:26 +0200

    Update _utils.py to add new to calculate_entropy
    Return max_entropy for commitment score plot as higher score = higher commitment

--- src/regvelo/metrics/_utils.py
+++ src/regvelo/metrics/_utils.py
@@ -37,7 +37,8 @@ def calculate_entropy(prob_matrix : np.ndarray) -> np.ndarray:
     np.log2(prob_matrix, where=mask, out=log_probs)
 
     entropy = -np.sum(prob_matrix * log_probs, axis=1)
-    return entropy
+    max_entropy = np.log2(prob_matrix.shape[1])  
+    return entropy, max_entropy
 
 def get_significance(pvalue: float) -> str:
     """Return significance annotation for a p-value.
